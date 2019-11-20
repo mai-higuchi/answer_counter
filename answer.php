d71b8b8dd734c6be02cee44e057a68ddf6eb47ad
mai-higuchi <k16jk107@st.kyusan-u.ac.jp>
2019-11-20 19:05:47 +0900

    テスト名、問題文修正

--- answer.php
+++ answer.php
@@ -1,138 +1,112 @@
 <?php
-require_once ('sql.php');  //接続
-$questionid = $_GET['questionid'];
-$testid = $_GET['testid']; //id取得
+require_once ('sql.php'); // 接続
+$questionid = $_GET ['questionid'];
+$testid = $_GET ['testid']; // id取得
 
-//SQL
+// SQL
+$sql0 = "SELECT DISTINCT questionid,a.id,questiontext,parent,name FROM mdl_question a,mdl_question_attempts b
+WHERE parent = 0 AND a.id=b.questionid AND questionid=$questionid";
 $sql = "SELECT questionid,rightanswer,questionsummary FROM mdl_question_attempts WHERE questionid='$questionid' GROUP BY questionid";
-$sql2= "SELECT questionid,responsesummary,questionsummary FROM mdl_question_attempts WHERE questionid = '$questionid' ORDER BY responsesummary";
-$i=0;
+$sql2 = "SELECT questionid,responsesummary,questionsummary FROM mdl_question_attempts WHERE responsesummary IS NOT NULL AND questionid = '$questionid' ORDER BY responsesummary";
+$i = 0;
+$stmt0 = $dbh->query ( $sql0);
 $stmt = $dbh->query ( $sql );
 $stmt2 = $dbh->query ( $sql2 );
-$part_answer=array();
-$syuukei = array();
-$first_answer=array();
-$answer = array();
-$answer2 = array();
-$first_answer2 = array();
-$first_answer3 = array();
-$first_answer4 = array();
-//解答データをすべて配列の中に入れるメソッド
+$part_answer = array ();
+$syuukei = array ();
+$answer = array ();
+// 解答データをすべて配列の中に入れるメソッド
 function array_flatten($array) {
-	if (!is_array($array)) {
+	if (! is_array ( $array )) {
 		return FALSE;
 	}
-	$result = array();
-	foreach ($array as $key3 => $val) {
-		if (is_array($val)) {
-			$result = array_merge($result, array_flatten($val));
+	$result = array ();
+	foreach ( $array as $key3 => $val ) {
+		if (is_array ( $val )) {
+			$result = array_merge ( $result, array_flatten ( $val ) );
 		} else {
-			$result[$key3] = $val;
+			$result [$key3] = $val;
 		}
 	}
 	return $result;
 }
-echo "テスト".$testid."<br>";
+//echo "テスト" . $testid . "<br>";
+while($result5 = $stmt0 ->fetch(PDO::FETCH_ASSOC)){
+	echo $result5['name'];
+	echo $result5['questiontext'];
+}
+echo "正答";
 
-//正答表作成
-while ($result = $stmt->fetch ( PDO::FETCH_ASSOC ) ) {
-	echo "問題<br>";
-	echo "<th>".$result['questionsummary']."</th>";
+
+// 正答表作成
+while ( $result = $stmt->fetch ( PDO::FETCH_ASSOC ) ) {
+	//echo "問題<br>";
+	//echo "<th>" . $result ['questionsummary'] . "</th>";
 
 	echo '<table border=2>';
 	$right_answer = explode ( ';', $result ['rightanswer'] );
 
-	foreach ($right_answer as $key => $value_right_answer ) {
+	foreach ( $right_answer as $key => $value_right_answer ) {
 		echo "<tr>";
-		echo "<td>".$value_right_answer."</td>";
+		echo "<td>" . $value_right_answer . "</td>";
 		echo "</tr>";
 	}
 	echo '</table>';
-
 }
-//解答データの分割
+
+// 解答データの分割
 echo '<br>';
 echo '解答集計結果<br>';
-while ($result2 = $stmt2->fetch ( PDO::FETCH_ASSOC ) ) {
+while ( $result2 = $stmt2->fetch ( PDO::FETCH_ASSOC ) ) {
 	$all_answer = explode ( ';', $result2 ['responsesummary'] );
-	//$answer[]=$all_answer;
+	// $answer[]=$all_answer;
 
-	$counter = count($all_answer);
-	//回答者数の数だけ配列を作成
+	$counter = count ( $all_answer );
 
-	for($i=0;$i<$counter;$i++){
-		echo "i:".$i.":<br>";
-		if(! isset($part_answer[$i])) {
-			$part_answer[$i] = array();
+	for($i = 0; $i < $counter; $i ++) {
+		//echo "i:" . $i . ":<br>";
+		if (! isset ( $part_answer [$i] )) {
+			$part_answer [$i] = array ();
 		}
-		array_push($part_answer[$i],array_shift($all_answer));
-		echo"<br>";
-		$answer = array_flatten($part_answer[$i]);
-		echo "<br>syuukei";
-		print_r($answer);
-		$syuukei = array_count_values($answer);
-		echo "<br>answercount";
-		print_r($syuukei);
-		echo "<br>part_answer_show";
-		print_r($part_answer[$i]);
-
-/*
-			$syuukeicounter = count($syuukei);
-			echo "<table border=2>";
-			foreach($syuukei as $key5 => $count2){
-				echo "<tr>";
-				echo "<td>".$key5."</td>";
-				echo "<td>".$count2."</td>";
-				echo "</tr>";
-
-			}
-			echo "</table>";
-			*/
+		array_push ( $part_answer [$i], array_shift ( $all_answer ) );
+		//echo "<br>";
 	}
 
-	echo "<br>part_answer";
-	print_r($part_answer);
-	//echo "<br>syuukei";
-	//print_r($syuukei);
-
+	//echo "<br>part_answer";
+	//print_r ( $part_answer );
 }
+// 解答数集計表作成
 
-
-/*
-echo "<table border=2>";
-foreach($syuukei as $key4 => $value_all_answer2){
-	var_dump($key4);
-	//var_dump($value_all_answer2);
-	echo "<tr>";
-	echo "<td>".$key4."</td>";
-	echo "<td>".$value_all_answer2."</td>";
-	echo "</tr>";
-}
-echo "</table>";
-
-
-//$answer = array_flatten($part_answer);
-//var_dump($answer);
-//解答数集計表作成
-*/
-
-foreach($part_answer as $array){
+foreach ( $part_answer as $array ) {
 	echo "<br>";
-	foreach($array as $key5 => $value_answer_array){
-		$syuukei = array_count_values($array);
-		echo "<br>";
-		print_r($syuukei);
+	foreach ( $array as $key5 => $value_answer_array ) {
+		$people = count($array);
+		$syuukei = array_count_values ($array);
+		//array_flip($syuukei);
+		//echo "<br>";
+		//print_r ($syuukei);
 	}
-		echo "<table border=2>";
-		echo "<br>";
-		foreach($syuukei as $key6 => $value_answer_array2){
+	echo "<table border=2>";
+	echo "<br>";
+	echo '<tr>';
+	echo '<th>解答結果</th>';
+	echo '<th>解答人数</th>';
+	echo '<th>率</th>';
+	echo '</tr>';
+
+	foreach ( $syuukei as $value_answer => $answer_count ) {
+		if(floor((($answer_count/$people)*100))>=40){
+			echo '<tr bgcolor="red">';
+		}else{
 			echo "<tr>";
-			echo "<td>".$value_answer_array2."</td>";
-			echo "<td>".$key6."</td>";
-			echo "</tr>";
 		}
-		echo "</table>";
+		echo "<td>" . $value_answer . "</td>";
+		echo "<td>" . $answer_count. "人</td>";
+		echo "<td>".floor((($answer_count/$people)*100))."%</td>";
+		echo "</tr>";
 	}
+	echo "</table>";
+}
 
 
 
--- home.php
+++ home.php
@@ -1,17 +1,21 @@
 <?php
 require_once ('sql.php');
-$questionid = $_GET['questionid'];
+//$questionid = $_GET['questionid'];
 
-$sql = 'SELECT questionid FROM mdl_question_attempts GROUP BY questionid';
+$sql = 'SELECT DISTINCT questionid,parent,name FROM mdl_question,mdl_question_attempts
+WHERE questionid = parent';
+
 $stmt = $dbh->query ( $sql );
-$i=0;
+
+echo "<th>テスト集計</th>";
+
 echo "<table border=2>";
 
 while ($result = $stmt->fetch ( PDO::FETCH_ASSOC ) ) {
-	$i++;
-	echo "<tr>";
-	echo "<td>"."テスト".$i."</td>";
-	echo '<td><a href="answer.php?questionid='.$result['questionid'].'&testid='.$i.'">詳細</td>"';
+
+	echo '<tr>';
+	echo '<td>'.$result['name'].'</td>';
+	echo '<td><a href="answer.php?questionid='.$result['questionid'].'">詳細</td>';
 	echo "</tr>";
 }
 echo "</table>";
